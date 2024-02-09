ae98b870659566ba7cfa25feebc9a45437edf9ee
Takeshi-Git <mr.takeshi.ueda@gmail.com>
2024-02-10 01:34:20 +0900

    Test04.csをTestGameObjectに加えた。 自宅作業。

---
+++ Assets/Scripts/Test04.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test04 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Test04");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
