1f06e0cd20241f3a252b61d9ad4a0ddff106bbd2
Takeshi-Git <mr.takeshi.ueda@gmail.com>
2024-02-11 13:14:02 +0900

    宿題。CuveMoverを作りキューブを移動。

---
+++ Assets/Scripts/CubeMover.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // ©g‚ÌˆÊ’uî•ñ‚ğæ“¾
+        Transform myTransform = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //X•ûŒü‚ÉˆÚ“®‚·‚é(‰E•ûŒü)
+        transform.Translate(1.0f * Time.deltaTime, 0, 0);
+
+    }
+}
